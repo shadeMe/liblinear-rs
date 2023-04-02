6914fc63ff18cca0d4ed17c220fc827403da68f3
shadeMe <shademehere@gmail.com>
2023-04-02 20:35:02 +0200

    Add `IsSupportVectorRegressionSolver` derive macro

--- crates/liblinear-macros/src/lib.rs
+++ crates/liblinear-macros/src/lib.rs
@@ -5,62 +5,71 @@ use syn::{parse_macro_input, DeriveInput};
 #[proc_macro_derive(IsLogisticRegressionSolver)]
 pub fn derive_is_logistic_regression_solver(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, .. } = parse_macro_input!(input);
-    let output = quote! {
+    quote! {
         impl traits::IsLogisticRegressionSolver for #ident {}
-    };
-    output.into()
+    }
+    .into()
+}
+
+#[proc_macro_derive(IsSupportVectorRegressionSolver)]
+pub fn derive_is_support_vector_regression_solver(input: TokenStream) -> TokenStream {
+    let DeriveInput { ident, .. } = parse_macro_input!(input);
+    quote! {
+        impl traits::IsSupportVectorRegressionSolver for #ident {}
+    }
+    .into()
 }
 
 #[proc_macro_derive(IsSingleClassSolver)]
 pub fn derive_is_single_class_solver(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, .. } = parse_macro_input!(input);
-    let output = quote! {
+    quote! {
         impl traits::IsSingleClassSolver for #ident {}
-    };
-    output.into()
+    }
+    .into()
 }
 
 #[proc_macro_derive(IsNonSingleClassSolver)]
 pub fn derive_is_non_single_class_solver(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, .. } = parse_macro_input!(input);
-    let output = quote! {
+    quote! {
         impl traits::IsNonSingleClassSolver for #ident {}
-    };
-    output.into()
+    }
+    .into()
 }
 
 #[proc_macro_derive(IsTrainableSolver)]
 pub fn derive_is_trainable_solver(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, .. } = parse_macro_input!(input);
-    let output = quote! {
+    quote! {
         impl traits::IsTrainableSolver for #ident {}
-    };
-    output.into()
+    }
+    .into()
 }
 
 #[proc_macro_derive(CanDisableBiasRegularization)]
 pub fn derive_can_disable_bias_regularization(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, .. } = parse_macro_input!(input);
-    let output = quote! {
+    quote! {
         impl traits::CanDisableBiasRegularization for #ident {}
-    };
-    output.into()
+    }
+    .into()
 }
 
 #[proc_macro_derive(SupportsInitialSolutions)]
 pub fn derive_supports_initial_solutions(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, .. } = parse_macro_input!(input);
-    let output = quote! {
+    quote! {
         impl traits::SupportsInitialSolutions for #ident {}
-    };
-    output.into()
+    }
+    .into()
 }
 
 #[proc_macro_derive(SupportsParameterSearch)]
 pub fn derive_supports_parameter_search(input: TokenStream) -> TokenStream {
     let DeriveInput { ident, .. } = parse_macro_input!(input);
-    let output = quote! {
+    quote! {
         impl traits::SupportsParameterSearch for #ident {}
-    };
-    output.into()
+    }
+    .into()
 }
